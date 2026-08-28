7d4605d281c1174a499ecdb4feb317804cb84aef
Paul Andrey Roldan <paulandreyabadroldan@gmail.com>
2026-08-28 19:44:44 +0800

    feat: configure student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'student_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'mobile_number',
+        'date_of_birth',
+        'gender',
+        'program',
+        'year_level',
+        'address',
+        'profile_picture',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
 }
