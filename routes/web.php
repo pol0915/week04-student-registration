8471ac6cd3f36ac167e81641c5d5181d314e1e0f
Paul Andrey Roldan <paulandreyabadroldan@gmail.com>
2026-08-28 19:48:50 +0800

    feat: add student registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StudentController::class, 'create'])
+    ->name('students.create');
+
+Route::post('/students', [StudentController::class, 'store'])
+    ->name('students.store');
+
+Route::get('/students', [StudentController::class, 'index'])
+    ->name('students.index');
+
+Route::get('/students/{student}', [StudentController::class, 'show'])
+    ->name('students.show');
